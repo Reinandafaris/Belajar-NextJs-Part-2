3d2e7175713210b16dc85758da3f4586c1d486b2
Reinandafaris <reinandafaris7@gmail.com>
2024-05-02 22:32:50 +0700

    first commit

---
+++ app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1 className="not-found">Page Not Found</h1>
+      <p>unfortunately, this page does not exist</p>
+    </main>
+  );
+}
